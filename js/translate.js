c69ef75711f49dc6fe9eddc74dc504129b9c5c4a
Christina Lau <tenniskeeper@gmail.com>
2019-11-26 18:00:44 -0500

    fix

--- js/translate.js
+++ js/translate.js
@@ -1,4 +1,31 @@
-
+/*
 function googleTranslateElementInit() {
-      new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+      new google.translate.TranslateElement({pageLanguage: 'en', 
+      	                                     layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
 }
+*/
+
+/**
+ * This will make the language selection at the bottom scrollable for responsive support
+ */
+function googleTranslateElementInit() {
+
+            new google.translate.TranslateElement({
+                pageLanguage: 'en',
+                autoDisplay: false,
+                layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+            }, 'google_translate_element');
+            function changeGoogleStyles() {
+                if($('.goog-te-menu-frame').contents().find('.goog-te-menu2').length) {
+                    $('.goog-te-menu-frame').contents().find('.goog-te-menu2').css({
+                        'max-width':'100%',
+                        'overflow-x':'auto',
+                        'box-sizing':'border-box',
+                        'height':'auto'
+                    });
+                } else {
+                    setTimeout(changeGoogleStyles, 50);
+                }
+            }
+            changeGoogleStyles();
+}   
